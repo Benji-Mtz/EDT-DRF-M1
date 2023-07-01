e56c5fc29002d65206395bf473faf02cb6d1e822
Benji Martinez <benjim.flores@outlook.com>
2023-06-30 21:41:22 -0600

    Editando la respuesta al cliente

--- todolist/serializers.py
+++ todolist/serializers.py
@@ -8,3 +8,13 @@ class TareaSerializer(serializers.ModelSerializer):
         # fields = ('campo_especifico1'...n)
         fields = '__all__'
         
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        if instance.estado == '1':
+            estado = "pendiente"
+        elif instance.estado == "2":
+            estado = "terminado"
+            
+        representation['estado'] = estado
+        
+        return representation
